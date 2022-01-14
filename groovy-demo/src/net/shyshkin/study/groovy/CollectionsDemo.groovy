811bd1d3762791f4b9e7542be71eb1ea3eace9cb
Art <d.art.shishkin@gmail.com>
2022-01-14 13:26:41 +0200

    20. Sets and Maps (#2)

--- groovy-demo/src/net/shyshkin/study/groovy/CollectionsDemo.groovy
+++ groovy-demo/src/net/shyshkin/study/groovy/CollectionsDemo.groovy
@@ -24,4 +24,35 @@ l.reverseEach { print it }
 println("\neachPermutation")
 ['a', 'b', 'c'].eachPermutation { println it }
 
+s = ['java', 'js', 'python', 'js'] as Set
+println s
+println s.class
+
+emptyMap = [:]
+println emptyMap
+println emptyMap.getClass()
+
+m = [courseName: 'Gradle', rating: 5, price: 9.99]
+println m
+println m.rating.class
+println m.price.class
+
+m.each { k, v ->
+    println "${k} -> ${v}"
+}
+
+// Access to fields
+println m.courseName
+println m['courseName']
+println m.get('courseName')
+
+//new key
+m.review = 'It\'s awesome'
+m['students'] = 4321
+m.put('enrolled', 567)
+println m
+
+TreeSet treeSet = [3, 2, 1, 5, 6] as Set
+println treeSet
+println treeSet.class
 
