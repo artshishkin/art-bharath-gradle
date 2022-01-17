a30269e5dc0bdca92800f01aa67f4fe227683558
Art <d.art.shishkin@gmail.com>
2022-01-17 14:32:51 +0200

    45.1 Test doGet - My Impl (#6)

--- coupon-app/web/src/test/java/net/shyshkin/study/gradle/CouponServletTest.java
+++ coupon-app/web/src/test/java/net/shyshkin/study/gradle/CouponServletTest.java
@@ -1,12 +1,20 @@
 package net.shyshkin.study.gradle;
 
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class CouponServletTest {
@@ -20,14 +28,20 @@ class CouponServletTest {
     @Mock
     RequestDispatcher requestDispatcher;
 
+    @Mock
+    PrintWriter printWriter;
+
     @Test
-    void doGet() {
+    void doGet() throws IOException, ServletException {
         //given
+        var couponServlet = new CouponServlet();
+        given(response.getWriter()).willReturn(printWriter);
 
         //when
+        couponServlet.doGet(request, response);
 
         //then
-
+        then(printWriter).should().print(eq("SUPERSALE"));
     }
 
     @Test
